7e7eae1883092755e00c2dbe3651bddc7e6ad062
Chip Cerio <alistair.cerio@gmail.com>
2018-05-21 09:13:41 +0800

    added runtime permission

--- app/src/main/java/com/chipcerio/audiorecorder/MainActivity.kt
+++ app/src/main/java/com/chipcerio/audiorecorder/MainActivity.kt
@@ -1,16 +1,36 @@
 package com.chipcerio.audiorecorder
 
+import android.Manifest
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import pub.devrel.easypermissions.AfterPermissionGranted
+import pub.devrel.easypermissions.EasyPermissions
 
 class MainActivity : AppCompatActivity() {
+
+    private val RC_AUDIO_PERM = 100
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onRequestPermissionsResult(rc: Int, perms: Array<out String>, res: IntArray) {
+        super.onRequestPermissionsResult(rc, perms, res)
+        EasyPermissions.onRequestPermissionsResult(rc, perms, res)
+    }
+
+    @AfterPermissionGranted(100) // because RC_AUDIO_PERM fails ¯\_(ツ)_/¯
+    private fun requestAudioPermission() {
+        val audioPermission = Manifest.permission.RECORD_AUDIO
+        if (EasyPermissions.hasPermissions(this, audioPermission)) {
+            // proceed
+        } else {
+            EasyPermissions.requestPermissions(this,
+                getString(R.string.audio_permission_rationale), RC_AUDIO_PERM, audioPermission)
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -20,7 +40,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
         if (item?.itemId == R.id.record_audio) {
-            Log.d("MainActivity", "clicked")
+            requestAudioPermission()
         }
         return super.onOptionsItemSelected(item)
     }
